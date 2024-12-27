d95fffa7c8b28fb81be423daf5b07592f0c5e8db
Ruan Vieira <ruanlendario12@gmail.com>
2024-12-27 10:34:17 -0300

    Aprendendo sobre variaveis

--- Dia 3/index.js
+++ Dia 3/index.js
@@ -1,18 +1,18 @@
 //informações das variaveis 
 
-let Nome = prompt();
-let idade = parseInt(prompt("Informe sua idade: "));
-let altura = parseFloat(prompt("Informe sua altura: "));
-let peso = parseInt(prompt("Informe seu peso: "));
+let Nome = "Ruan Vieira"
+let idade = 25
+let altura = 1.73
+let peso = "63 kg"
 
-//
+//Atruibuindo as informações do usuario, valores e nomes.
 
-console.log(Nome)
-console.log(idade) 
-console.log(altura)
-console.log(peso) 
+Nome = prompt("Informe seu nome; ")
+idade = parseInt(prompt("Informe sua idade: "));
+altura = parseFloat(prompt("Informe sua altura: "));
+peso = parseInt(prompt("Informe seu peso: "));
 
-//
+//Calculando o ano em  que a pessoa nasceu e o IMC
 
 let anoNasc = 0
 anoNasc = 2023 - idade
@@ -20,7 +20,7 @@ anoNasc = 2023 - idade
 let imc = 0
 imc = peso / (altura*altura)
 
-//
+//Informações disponiveis no console 
 
-console.log (" Óla " + Nome + " , Voce tem " + idade + " anos , nasceu em " + 1998 + ", tem" + altura + " de altura, pesa "
+console.log (" Óla " + Nome + " , Voce tem " + idade + " anos , nasceu em " + 1998 + " , tem" + altura + " de altura, pesa "
      + peso + " kg seu imc è " + imc + " kg/m² ")
