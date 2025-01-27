ad788f703cf5a95a3088bed69c0c7ad7c6cc0daa
Ruan Vieira <ruanlendario12@gmail.com>
2025-01-27 18:40:37 -0300

    Desafio do dia 4- IF/ELSE motorista

--- Dia 4/index.js
+++ Dia 4/index.js
@@ -1,15 +1,14 @@
+let Estacomfome = prompt('Você esta com fome? (sim/não)')
+let Temdinheiro = prompt('Você tem dinheiro ? (sim/não)') 
+let Abetorestaurante = prompt('O restaurante está aberto ? (sim/não)')
 
-const estacomFome  = prompt(" Você está com fome? (sim/não)")
-const estacomDinheiro = prompt(" Você tem dinheiro? (sim/não)")
-const restauranteAberto = prompt(" O restaurante está aberto? (sim/não)")
-
-if (estacomFome === "não" || estacomDinheiro == "não" ) {
-    console.log("Hoje a janta será em casa");
-}else if (estacomFome === "sim" && restauranteAberto === "não" )
+if(Estacomfome === 'não' || Temdinheiro === 'não'){
+    console.log("Hoje a janta será em casa")
+}else if(Estacomfome === 'sim' && Temdinheiro === 'sim' && Abetorestaurante === 'sim'){
+    console.log("Hoje o janta será no seu restaurante preferido")
+} if(Estacomfome === 'sim' && Temdinheiro === 'sim' && Abetorestaurante === 'não'){
     console.log("Peça um delivery")
-else {
-        console.log("Hoje o jantar será no seu restaurante preferido")
-    }
+}
 
 
 
